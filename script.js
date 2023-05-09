8e3b737714d4aa3cca55a884567cb3fd778be80b
daaron97 <danbalch@outlook.com>
2023-05-09 21:21:11 +0100

    Made skills section interactive

---
+++ script.js
@@ -0,0 +1,11 @@
+var currentSkill = 1; // current skill being displayed
+    function showNextSkill() {
+        // hide the current skill
+        document.getElementById("skill-" + currentSkill).style.display = "none";
+        
+        // increment the current skill counter and wrap around if it exceeds the number of skills
+        currentSkill = (currentSkill % 4) + 1;
+        
+        // show the next skill
+        document.getElementById("skill-" + currentSkill).style.display = "block";
+    }
